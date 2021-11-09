5186723664c8d117d986765ef70d5e9e1687e7df
ZNBrown <ZachNatBrown@gmail.com>
2021-11-09 15:41:59 +0000

    adds stub pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header, Footer } from './layout';
+import { Switch, Route } from 'react-router-dom';
+import * as Pages from './pages';
+
 
 
 function App() {
   return (
     <>
     <Header />
-    
+    <Switch>
+
+    <Route exact path="/">
+          <Pages.About />
+    </Route>
+    <Route path="/about">
+          <Pages.About />
+    </Route>
+
+    <Route>
+          <h1>Oops, that path doesn't exist</h1>
+        </Route>
+    </Switch>
     <Footer />
     </>
   );
---
+++ my-app/src/pages/index.js
@@ -0,0 +1,3 @@
+export {About} from "./About"
+// export {Skills} from "./Skills"
+// export {Projects} from "./Projects"
